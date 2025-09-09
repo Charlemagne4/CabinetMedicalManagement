431b613a44a3d85b5af9add920bb8fbb0f347989
Charlemagne4 <mohdingo4@gmail.com>
2025-09-09 17:31:37 +0100

    feat(auth): start of auth logic and familiarize with auth V5 files

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,7 +2,11 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
+import Credentials from "next-auth/providers/credentials";
+
 import { db } from "@/server/db";
+import { prisma } from "prisma/prisma";
+import { comparePasswords } from "@/utils/passwordHasher";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -32,6 +36,58 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
+    Credentials({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Chaklamo" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "******",
+        },
+      },
+      authorize: async (credentials) => {
+        console.log(credentials);
+        // Add logic here to look up the user from the credentials supplied
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        console.log("AAAAA");
+        if (!user) return null;
+
+        console.log("AAAAA");
+        console.log(!user.password);
+        console.log(!user.salt);
+        console.log(!credentials?.password);
+        if (!user.password || !user.salt || !credentials?.password) {
+          // logger.warn("Missing credentials or user data during login attempt");
+          return null;
+        }
+        console.log("AAAAA");
+        if (
+          await comparePasswords({
+            hashedPassword: user.password,
+            password: credentials.password,
+            salt: user.salt,
+          })
+        ) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          console.log("AAAAA");
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
     DiscordProvider,
     /**
      * ...add more providers here.
